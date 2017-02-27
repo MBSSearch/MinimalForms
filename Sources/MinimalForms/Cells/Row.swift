403589a38414459d6256de1b411faf1e323d1510
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-27 20:08:58 +1100

    Define Row enum
    This enum lists the types of rows available. Each case matches one of
    our custom `UITableViewCell` subclqsses.

---
+++ Sources/MinimalForms/Cells/Row.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+/// The availalbe kind of rows.
+public enum Row {
+
+  case detail(
+    text: String,
+    detail: String,
+    afterConfigure: ((DetailCell) -> ())?,
+    didSelect: (() -> ())?
+  )
+
+  case `switch`(
+    text: String,
+    detail: String?,
+    didChange: ((UISwitch) -> ()),
+    afterConfigure: ((SwitchCell) -> ())?,
+    didSelect: (() -> ())?
+  )
+
+  case button(
+    title: String,
+    alignment: NSTextAlignment,
+    didSelect: (() -> ())?,
+    afterConfigure: ((ButtonCell) -> ())?
+  )
+
+  case textField(
+    placeholder: String?,
+    value: String?,
+    didChange: ((UITextField) -> ()),
+    afterConfigure: ((TextFieldCell) -> ())?,
+    didSelect: (() -> ())?
+  )
+}
+
+extension Row {
+
+  public var identifier: String {
+    switch self {
+    case .detail: return "detail"
+    case .switch: return "switch"
+    case .button: return "button"
+    case .textField: return "textfiel"
+    }
+  }
+
+  public var cellClass: UITableViewCell.Type {
+    switch self {
+    case .detail: return DetailCell.self
+    case .switch: return SwitchCell.self
+    case .button: return ButtonCell.self
+    case .textField: return TextFieldCell.self
+    }
+  }
+
+  public var didSelect: (() -> ())? {
+    switch self {
+    case .button(_, _, let _didSelect, _): return _didSelect
+    case .detail(_, _, _, let _didSelect): return _didSelect
+    case .switch(_, _, _, _, let _didSelect): return _didSelect
+    case .textField(_, _, _, _, let _didSelect): return _didSelect
+    }
+  }
+
+  public func configure(_ cell: UITableViewCell) {
+    switch self {
+
+    case .detail(let text, let detail, let afterConfigure, _):
+      guard let _cell = cell as? DetailCell else { return }
+      _cell.textLabel?.text = text
+      _cell.detailTextLabel?.text = detail
+      afterConfigure?(_cell)
+
+    case .switch(let text, let detail, let didChange, let afterConfigure, _):
+      guard let _cell = cell as? SwitchCell else { return }
+      _cell.textLabel?.text = text
+      _cell.detailTextLabel?.text = detail
+      _cell.didChange = didChange
+      afterConfigure?(_cell)
+
+    case .button(let title, let alignment, _, let afterConfigure):
+      guard let _cell = cell as? ButtonCell else { return }
+      _cell.textLabel?.text = title
+      _cell.textLabel?.textAlignment = alignment
+      afterConfigure?(_cell)
+
+    case .textField(let placeholder, let value, let didChange, let afterConfigure, _):
+      guard let _cell = cell as? TextFieldCell else { return }
+      _cell.textField.placeholder = placeholder
+      _cell.textField.text = value
+      _cell.didChange = didChange
+      afterConfigure?(_cell)
+    }
+  }
+}
