f9e23102f581232d3fa04fb4df50731b600449ff
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-03-23 08:24:24 +1100

    Add stateful hack to avoid button cell color changing

--- Sources/MinimalForms/ButtonCell.swift
+++ Sources/MinimalForms/ButtonCell.swift
@@ -3,6 +3,8 @@ import UIKit
 /// A cell that can be used as an iOS 7 style text button, with configurable text alignment.
 public class ButtonCell: UITableViewCell {
 
+  private var didSetTintColor = false
+
   public var buttonTextAlignment: NSTextAlignment = .left {
     didSet(newValue) {
       textLabel?.textAlignment = newValue
@@ -19,7 +21,17 @@ public class ButtonCell: UITableViewCell {
 
   public override func layoutSubviews() {
     super.layoutSubviews()
+
+    // This is an hack due to the fact that the superview tintColor changes during
+    // selection/highlight but because that is an animated change this methog somehow doesn't get
+    // called at completion, so we are stuck with the label being grey after selection.
+    //
+    // The current solution for this issue then is to pick the superview tint color only once, and
+    // avoid resetting it.
+    guard didSetTintColor == false else { return }
+
     guard let superview = superview else { return }
+    didSetTintColor = true
     textLabel?.textColor = superview.tintColor
   }
 }
